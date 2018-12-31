798087ff52e20552f0d8fb20f93e33c97592fe33
Willie Fatimehin <willie.fatimehin@gmail.com>
2018-12-30 19:00:22 -0500

    initial xcode project

---
+++ dev_profile/dev_profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev_profile
+//
+//  Created by Willie Fatimehin on 12/30/18.
+//  Copyright © 2018 Willie Fatimehin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
